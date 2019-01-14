4992c89729ef2ad41f49e2e713ea012b1a544203
Hoa Vo <vovanhoait@gmail.com>
2019-01-14 18:55:42 +0700

    Update new verison plugin

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -13,7 +13,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.2",
-  "popdeem-cordova-plugin": "0.1.15"
+  "popdeem-cordova-plugin": "0.1.16"
 };
 // BOTTOM OF METADATA
 });
