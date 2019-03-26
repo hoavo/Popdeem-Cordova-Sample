c35fd87282c891ce88de5afd36319ef139dbf181
Hoa Vo <vovanhoait@gmail.com>
2019-03-26 18:32:45 +0700

    update code

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -13,7 +13,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.2",
-  "popdeem-cordova-plugin": "0.1.16"
+  "popdeem-cordova-plugin": "0.1.30"
 };
 // BOTTOM OF METADATA
 });
--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,19 +1,16 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-  {
-    "id": "popdeem-cordova-plugin.popdeem",
-    "file": "plugins/popdeem-cordova-plugin/www/popdeem.js",
-    "pluginId": "popdeem-cordova-plugin",
-    "clobbers": [
-      "popdeem"
-    ]
-  }
-];
-module.exports.metadata = 
-// TOP OF METADATA
-{
-  "cordova-plugin-whitelist": "1.3.2",
-  "popdeem-cordova-plugin": "0.1.16"
-};
-// BOTTOM OF METADATA
+  module.exports = [
+    {
+      "id": "popdeem-cordova-plugin.popdeem",
+      "file": "plugins/popdeem-cordova-plugin/www/popdeem.js",
+      "pluginId": "popdeem-cordova-plugin",
+      "clobbers": [
+        "popdeem"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.2",
+    "popdeem-cordova-plugin": "0.1.30"
+  };
 });
